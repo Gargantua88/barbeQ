9797f42b0d7955c3c192ff9e74c9e0bbff521b2c
gargantua <istroganovi@gmail.com>
2018-12-13 17:18:35 +0300

    Add comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :events
+  has_many :comments
 
   validates :name, presence: true, length: {maximum: 35}
 end
