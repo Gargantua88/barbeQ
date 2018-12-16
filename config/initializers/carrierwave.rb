f48f0d602281f62ae9d2f4403589e7550ce54b0f
gargantua <istroganovi@gmail.com>
2018-12-17 01:14:51 +0300

    try AWS

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,14 @@
 require 'carrierwave/storage/fog'
 
-if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-        provider:              'AWS',
-        aws_access_key_id:     ENV['S3_ACCESS_KEY'],
-        aws_secret_access_key: ENV['S3_SECRET_KEY']
-    }
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'                        # required
+  config.fog_credentials = {
+      provider:              'AWS',                        # required
+      aws_access_key_id:     'AKIAJAFVSWEDCVXPBRVQ',                        # required unless using use_iam_profile
+      aws_secret_access_key: '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',    # required unless using use_iam_profile
 
-    config.fog_directory  = ENV['S3_BUCKET_NAME']
-  end
+  }
+  config.fog_directory  = 'gargantua-bbq-bucket'                                      # required
+  config.fog_public     = true                                                 # optional, defaults to true
+  config.fog_attributes = { } # optional, defaults to {}
 end
