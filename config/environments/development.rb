08bac2e8723e12219c801dea84651dbc73510bd2
gargantua <istroganovi@gmail.com>
2018-12-18 14:06:33 +0300

    Add mailer functional 4 photo notify

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,8 +66,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address: 'smtp.gmail.com',
       port: '587',
-      user_name: ENV['GOOGLE_USERNAME'],
-      password: ENV['GOOGLE_PASSWORD'],
+      user_name: 'gargantuabot',
+      password: 'fuckingpassword',
       authentication: 'plain',
       enable_starttls_auto: true
   }
